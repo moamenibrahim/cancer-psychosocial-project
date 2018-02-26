f36f6b31df7b873bf40b837866a594016edf8bb5
mibrahem <mmibrahem76@gmail.com>
2018-02-26 04:33:11 +0200

    tweepy edits

--- twitter-test/StreamListener.py
+++ twitter-test/StreamListener.py
@@ -1,5 +1,6 @@
 import sys
 import tweepy
+import os 
 
 consumer_key = 'zgwY6GgJ2p6kCX39X17zm4UpK'
 consumer_secret = 'Kv9AazgJmYueIQPmY5kO1MhUsZvDiXaHJZw03fVe9p8H5AipPv'
@@ -9,12 +10,16 @@ access_secret = 'wHQFCa7MedvYkF9jtWNtu6rpGMOCXQR7Ptq5jsFKrAbEv'
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
+fName="output.txt"
+f= open(fName,"w+")
 
+## source: https://www.cancer.gov/types
+terms=["cancer","Tumor","Leukemia","Neuroblastoma","Paraganglioma","Retinoblastoma","Astrocytomas","Retinoblastoma","Lymphoma","Melanoma"]
 
 class CustomStreamListener(tweepy.StreamListener):
     def on_status(self, status):
-        if "cancer" in status.text.lower():
-            print (status.text)
+        print (status.text)
+        f.write(status.text)
 
     def on_error(self, status_code):
         print >> sys.stderr, 'Encountered error with status code:', status_code
@@ -25,6 +30,4 @@ class CustomStreamListener(tweepy.StreamListener):
         return True # Don't kill the stream
 
 sapi = tweepy.streaming.Stream(auth, CustomStreamListener())    
-sapi.filter(locations=[4.10,54.44,31.46,71.15])
-
-print(sapi)
+sapi.filter(track=["cancer" or "Tumor" or "Leukemia" or "Neuroblastoma" or "Paraganglioma" or "Retinoblastoma" or "Astrocytomas" or "Retinoblastoma" or "Lymphoma" or "Melanoma"],locations=[4.10,54.44,31.46,71.15])

45b287274fd56dd34d2075621b04b380b02830c2
mibrahem <mmibrahem76@gmail.com>
2018-02-28 03:22:06 +0200

    genia edit

--- geniatagger-test/geniatagger-runner.py
+++ geniatagger-test/geniatagger-runner.py
@@ -1,11 +1,15 @@
 # pip install geniatagger
-# or download and install from: https://pypi.python.org/pypi/geniatagger-python/0.1 
-# python setup.py install 
+# or download and install from: https://pypi.python.org/pypi/geniatagger-python/0.1
+# python setup.py install
 import geniatagger
 
-# Path to the original geniatagger downloaded from: http://www.nactem.ac.uk/GENIA/tagger/ 
+# Path to the original geniatagger downloaded from: http://www.nactem.ac.uk/GENIA/tagger/
 # Path should go as follows: path/geniatagger
-tagger = geniatagger.GeniaTagger('.../path_to_geniatagger/geniatagger')
+tagger = geniatagger.GeniaTagger(
+    '/home/moamen/work/cancer_project/geniatagger-3.0.2/geniatagger')
 
-# input text 
+# input text
 print tagger.parse('This is a pen.')
+
+
+#  Chunks are represented in the IOB2 format (B for BEGIN, I for INSIDE, and O for OUTSIDE).

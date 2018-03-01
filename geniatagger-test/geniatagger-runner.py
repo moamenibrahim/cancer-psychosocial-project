bd8e16e0a43af5be92633a7d2094b80cb8966c62
mibrahem <mmibrahem76@gmail.com>
2018-03-01 19:51:14 +0200

    edits to twitter streamlistener

--- geniatagger-test/geniatagger-runner.py
+++ geniatagger-test/geniatagger-runner.py
@@ -1,11 +1,10 @@
-# pip install geniatagger
-# or download and install from: https://pypi.python.org/pypi/geniatagger-python/0.1 
+# Download and install from: https://pypi.python.org/pypi/geniatagger-python/0.1 
 # python setup.py install 
 import geniatagger
 
 # Path to the original geniatagger downloaded from: http://www.nactem.ac.uk/GENIA/tagger/ 
 # Path should go as follows: path/geniatagger
-tagger = geniatagger.GeniaTagger('.../path_to_geniatagger/geniatagger')
+tagger = geniatagger.GeniaTagger('/home/mibrahem/work/geniatagger-3.0.2/geniatagger')
 
 # input text 
 print tagger.parse('This is a pen.')

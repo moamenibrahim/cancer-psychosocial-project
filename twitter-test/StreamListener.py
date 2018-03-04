b20b2653cd7ce23a6b058f5f91e7984c918bc5c6
mibrahem <mmibrahem76@gmail.com>
2018-03-04 16:47:42 +0200

    tweets output

--- twitter-test/StreamListener.py
+++ twitter-test/StreamListener.py
@@ -3,6 +3,8 @@ import tweepy
 import os 
 import json
 from subprocess import call 
+from datetime import datetime
+
 
 consumer_key = 'zgwY6GgJ2p6kCX39X17zm4UpK'
 consumer_secret = 'Kv9AazgJmYueIQPmY5kO1MhUsZvDiXaHJZw03fVe9p8H5AipPv'
@@ -12,13 +14,8 @@ access_secret = 'wHQFCa7MedvYkF9jtWNtu6rpGMOCXQR7Ptq5jsFKrAbEv'
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
-fName ="output.txt"
-f = open(fName,"w+")
 
-## source: https://www.cancer.gov/types
-## use track and include OR in between 
-terms=["cancer","Tumor","Leukemia","Neuroblastoma","Paraganglioma","Retinoblastoma","Astrocytomas","Retinoblastoma","Lymphoma","Melanoma"]
-
+outputDir = "outputDir"
 
 class CustomStreamListener(tweepy.StreamListener):
 
@@ -27,12 +24,20 @@ class CustomStreamListener(tweepy.StreamListener):
     '''
 
     def __init__(self):
-        self.tweet_count = 0
-        pass
+        super(CustomStreamListener,self).__init__(self)
+        self.basePath = outputDir
+        os.system("mkdir -p %s"%(outputDir))
 
-    def on_status(self, status):
+        d = datetime.today()
+        self.filename = "%i-%02d-%02d.json"%(d.year,d.month,d.day)
+        self.fh = open(self.basePath + "/" + self.filename,"a")#open for appending just in case
+
+        self.errorCount=0
+        self.limitCount=0
+        self.last=datetime.now()
+        self.tweet_count = 0
         return
-        
+
     def on_data(self,data):
         tweet_data = json.loads(data)
         if("extended_tweet") in tweet_data:
@@ -40,8 +45,26 @@ class CustomStreamListener(tweepy.StreamListener):
             tweet= tweet_data['extended_tweet']['full_text']
             print(self.tweet_count)
             print(tweet)
-            f.write(tweet)
-        return
+            self.fh.write(data)
+            self.status()         
+        else:
+            self.tweet_count = self.tweet_count+ 1
+            tweet= tweet_data['text']
+            print(self.tweet_count)
+            print(tweet)
+            self.fh.write(data)
+            self.status()
+        return True
+        
+    def status(self):
+        now=datetime.now()
+        if (now-self.last).total_seconds()>300:
+            print("%s - %i tweets, %i limits, %i errors in previous five minutes."%(now,self.tweet_count,self.limitCount,self.errorCount))
+            self.tweetCount=0
+            self.limitCount=0
+            self.errorCount=0
+            self.last=now
+            self.rotateFiles()#Check if file rotation is needed
 
     def on_error(self, status_code):
         if status_code == 420: #Returning False in on_data disconnects the stream
@@ -53,8 +76,30 @@ class CustomStreamListener(tweepy.StreamListener):
         print >> sys.stderr, 'Timeout...'
         return True # Don't kill the stream
 
+    def rotateFiles(self):
+        print("rotating file")
+        d=datetime.today()
+        filenow = "%i-%02d-%02d.json"%(d.year,d.month,d.day)
+        if (self.filename!=filenow):
+            print("%s - Rotating log file. Old: %s New: %s"%(datetime.now(),self.filename,filenow))
+            try:
+                self.fh.close()
+            except:
+                pass
+            self.filename=filenow
+            self.fh = open(self.basePath + "/" + self.filename,"a")
+            
+    def close(self):
+        try:
+            self.fh.close()
+            print("files closed")
 
-sapi = tweepy.streaming.Stream(auth, CustomStreamListener(), tweet_mode= 'extended')    
-sapi.filter(locations=[2.43,55.30,36.58,70.39], async=True)
+        except:
+            pass
 
-
+if __name__ == "__main__":
+        try: 
+            sapi = tweepy.streaming.Stream(auth, CustomStreamListener(), tweet_mode= 'extended')    
+            sapi.filter(locations=[2.43,55.30,36.58,70.39],async = True)
+        except KeyboardInterrupt:
+            sapi.close()

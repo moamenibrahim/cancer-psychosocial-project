fe7b1d4e74e4296b6533f387b6f9cdb6f0741bc0
mibrahem <mmibrahem76@gmail.com>
2018-03-06 12:51:06 +0200

    edits

--- geniatagger-test/geniatagger-runner.py
+++ geniatagger-test/geniatagger-runner.py
@@ -4,16 +4,19 @@ import geniatagger
 import os 
 # Path to the original geniatagger downloaded from: http://www.nactem.ac.uk/GENIA/tagger/
 # Path should go as follows: path/geniatagger
-tagger = geniatagger.GeniaTagger('/home/mibrahem/work/geniatagger-3.0.2/geniatagger')
+tagger = geniatagger.GeniaTagger('/home/moamen/work/cancer_project/geniatagger-3.0.2/geniatagger')
 
-f1 = open("sample.txt","r")
-f = open("output.txt","w+")
-output=f1.read()
+fread = open("sample.txt","r")
+fwrite = open("output.txt","w+")
+
+for line in fread.readlines():
+    # line_text=fread.readline()
+    out = tagger.parse(line)
+    fwrite.writelines(str(out)+'\n')
+
 
 # input text
-print tagger.parse('Cancer Inhibition of NF-kappaB activation reversed the anti-apoptotic effect of isochamaejasmin.')
-print tagger.parse('cancer Tumor Leukemia Neuroblastoma Paraganglioma Retinoblastoma Astrocytomas Retinoblastoma Lymphoma Melanoma')
-out=tagger.parse(output)
-f.write(str(out))
+# print tagger.parse('Cancer Inhibition of NF-kappaB activation reversed the anti-apoptotic effect of isochamaejasmin.')
+# print tagger.parse('cancer Tumor Leukemia Neuroblastoma Paraganglioma Retinoblastoma Astrocytomas Retinoblastoma Lymphoma Melanoma')
 
 #  Chunks are represented in the IOB2 format (B for BEGIN, I for INSIDE, and O for OUTSIDE).
--- twitter-test/StreamListenertest.py
+++ twitter-test/StreamListenertest.py
@@ -31,4 +31,5 @@ if __name__ == '__main__':
     auth.set_access_token(access_token, access_token_secret)
 
     stream = Stream(auth, l)
-stream.filter(track=['basketball'])
+
+stream.filter(track=["cancer" or "Tumor" or "Leukemia" or "Neuroblastoma" or "Paraganglioma" or "Retinoblastoma" or "Astrocytomas" or "Retinoblastoma" or "Lymphoma" or "Melanoma"])

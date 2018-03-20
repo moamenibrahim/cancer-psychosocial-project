b65cc29dd39891180bfec8121e4f8f4f7782fa01
mibrahem <mmibrahem76@gmail.com>
2018-03-20 10:43:47 +0200

    changes for twitter test

--- twitter-test/lda_topic.py
+++ twitter-test/lda_topic.py
@@ -25,27 +25,17 @@ class lda_modeling(object):
 		stop = set(stopwords.words('english'))
 		exclude = set(string.punctuation)
 		lemma = WordNetLemmatizer()
-		
 		norm = []
-		for doc in doc_complete:
-			stop_free = " ".join([i for i in doc.lower().split() if i not in stop])
-			punc_free = ''.join(ch for ch in stop_free if ch not in exclude)
-			normalized = " ".join(lemma.lemmatize(word) for word in punc_free.split())
-			norm.append(normalized.split())
+		stop_free = [i for i in doc_complete.lower().split() if i not in stop]
+		punc_free = ' '.join(ch for ch in stop_free if ch not in exclude)
+		normalized = " ".join(lemma.lemmatize(word) for word in punc_free.split())
+		norm.append(normalized.split())
 		return norm
 
-	def generate_topic(self):
-
-		doc_complete = []
-		with open ("input.txt", "r") as tpfile:
-			for line in tpfile:
-				term = line
-				doc_complete.append(term)
-
-		texts = self.preprocess_sentences(doc_complete)
+	def generate_topic(self, input):
+		texts = self.preprocess_sentences(input)
 		dictionary = corpora.Dictionary(texts)
 		doc_term_matrix = [dictionary.doc2bow(text) for text in texts]
-
 		Lda = gensim.models.ldamodel.LdaModel
 		ldamodel = Lda(doc_term_matrix, num_topics=3, id2word = dictionary, passes=300)
 		str = ldamodel.print_topics(num_topics=3, num_words=3)
@@ -54,8 +44,4 @@ class lda_modeling(object):
 		result = re.findall('[a-zA-Z]+',t)
 		print('topics from user input', result)
 
-		file = open("lda_topict.txt", "w")
-		for i in range(0, len(result)):
-			file.write(result[i]+"\n") 
-		file.close()
 		return result
--- twitter-test/tweets_processing.py
+++ twitter-test/tweets_processing.py
@@ -1,46 +1,64 @@
 import os 
 import sys 
 import json
+import re
 import geniatagger
+import string
 import time
+from nltk.stem.wordnet import WordNetLemmatizer
 from gensim import corpora, models, similarities
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from lda_topic import lda_modeling
 from ibmNLPunderstanding import AlchemyNLPunderstanding
+from googletrans import Translator
 
-fwrite = open("output.txt","w+")
+translator = Translator()
 
 mylist = [  "cancer",
-            "Tumor",
-            "Leukemia",
-            "Neuroblastoma",
-            "Paraganglioma",
-            "Retinoblastoma",
-            "Astrocytomas",
-            "Retinoblastoma",
-            "Lymphoma",
-            "Melanoma",
+            "tumor",
+            "leukemia",
+            "neuroblastoma",
+            "paraganglioma",
+            "retinoblastoma",
+            "astrocytomas",
+            "retinoblastoma",
+            "lymphoma",
+            "melanoma",
             "syöpä",
             "kasvain",
             "säteily",
             "hoito",
             "kuolla",
-            "Cancer",
             "Malignant",  # Refers to a tumor that is cancerous
-            "Metastasis", # Cancer spreading
-            "Oncology",   # Study of cancer 
-            "Oncologist",
-            "Pathologist",
+            "metastasis", # Cancer spreading
+            "oncology",   # Study of cancer 
+            "oncologist",
+            "pathologist",
             # Refers to cells that have the potential to become cancerous.
-            "Precancerous",
+            "precancerous",
             # A cancer that develops in the tissues that support and connect the body, such as fat and muscle.
-            "Sarcoma"]
+            "Sarcoma",
+            "telemedicine",
+            "healthcare",
+            "chemotherapy",
+            "mammogram"]
 
 family_list = [ "family",
                 "sad",
                 "love",
-                "hate"]
+                "hate",
+                "father",
+                "son",
+                "daughter",
+                "mother"]
+
+friend_list =["friends",
+              "friendship"]
+              
+money_list =["money",
+             "price"]
+
 
 lda = lda_modeling()
 
@@ -49,31 +67,66 @@ stop_words = set(stopwords.words('english'))
 NLP_understanding = AlchemyNLPunderstanding()
 
 def remove_stopWords(tweet):
+    norm = []
+    exclude = set(string.punctuation)
     word_tokens = word_tokenize(tweet)
     filtered_sentence = [w for w in word_tokens if not w in stop_words]
-    filtered_sentence = []
     for w in word_tokens:
         if w not in stop_words:
             filtered_sentence.append(w)
-    print(filtered_sentence) 
-    fwrite.write(str(filtered_sentence))
+    punc_free = ' '.join(ch for ch in filtered_sentence if ch not in exclude)
+    print(punc_free)
     return filtered_sentence
 
+    # stopwords_file = open("./finnish_stopwords.txt", "r")
+	# lines = stopwords_file.read().split(",")
+	# stopwords = lines[0].split("\n")
+	# tokenizer = RegexpTokenizer(r'\w+')
+	# texts = []
+	# for doc in docs:
+	# 	raw = doc.decode().lower()
+	# 	tokens = tokenizer.tokenize(raw)
+	# 	stopped_tokens = [i for i in tokens if not i in stopwords and len(i) != 1]
+	# 	texts.append(stopped_tokens)
+
 def get_topic(input_str):
-    with open("input.txt", "a") as file:
-        file.write(input_str+"\n")
-    topic = lda.generate_topic()
-    print(topic)
-    if any(word in topic for word in family_list):
-        get_sentiment(input_str)
+    try:
+        topic = lda.generate_topic(input_str)
+        if any(word in topic for word in family_list):
+            print("family related tweet: extracting emotions")
+            get_sentiment(input_str)
+        
+        if any(word in topic for word in friend_list):
+            print("friend related tweet: extracting emotions")
+            get_sentiment(input_str)
+
+        if any(word in topic for word in money_list):
+            print("money related tweet")
+            get_sentiment(input_str)
+        return True
+    except:
+        print("Error getting topic")
+    
+
+def get_translate(input_str,lang):
+    if(lang!='und'):
+        translated=translator.translate(input_str, dest='en', src=lang)
+        print(translated)
+        return translated.text
 
 def get_pos(tweet):
     out = tagger.parse(tweet)
     print(out)
-    fwrite.write(str(out)+'\n')
 
 def get_sentiment(input_str):
     NLP_understanding.get_response(input_str)
+
+def extract_link(text):
+    regex = r'https?://[^\s<>"]+|www\.[^\s<>"]+'
+    match = re.search(regex, text)
+    if match:
+        return match.group()
+    return ''
 
 def analyze_file(fileName, tweet_count):
     for line in fileName.readlines():
@@ -86,7 +139,15 @@ def analyze_file(fileName, tweet_count):
                 print(tweet)
                 print(tweet_data['lang'])
                 result=remove_stopWords(tweet)
-                get_topic(''.join(result))
+                try:
+                    translated=get_translate(' '.join(result), tweet_data['lang'])
+                    pass
+                except:
+                    print("Failed to translate text")
+                    pass
+
+                get_topic(translated)
+                time.sleep(0.8)
             
         else:
             tweet = tweet_data['text']
@@ -96,14 +157,20 @@ def analyze_file(fileName, tweet_count):
                 print(tweet)
                 print(tweet_data['lang'])
                 result= remove_stopWords(tweet)
-                get_topic(''.join(result))
+                try:
+                    translated=get_translate(' '.join(result), tweet_data['lang'])
+                except:
+                    print("Failed to translate text")
+                
+                get_topic(translated)
+                time.sleep(0.8)
 
 
 if __name__ == "__main__":
     tweet_count = 0
     tagger = geniatagger.GeniaTagger(
-        '/home/moamen/work/cancer_project/geniatagger-3.0.2/geniatagger')
-    time.sleep(6.5)
-    for x in range(3,6):
+        '/home/moamen/work/cancer_material/geniatagger-3.0.2/geniatagger')
+    time.sleep(7)
+    for x in range(3,7):
         fread = open("outputDir/2018-03-0"+str(x)+".json", "r")
         analyze_file(fread,tweet_count)

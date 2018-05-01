7a94cfeacff8b0c81c6e1ab2e1dd16057d778249
Moamen <mmibrahem76@gmail.com>
2018-05-02 02:50:44 +0300

    visualizing results

--- twitter-test/visualization.py
+++ twitter-test/visualization.py
@@ -25,47 +25,6 @@ staged_dict = {}
 for line in f.readlines():
     tweet_data = json.loads(line)
 
-    # Dictionary - sucess rate bar 
-    dict_result = tweet_data['dict_result']
-    print(dict_result)
-    rounded = round(dict_result)
-        if (rounded in staged_dict):
-            ## increment that rounded
-            staged_dict[rounded] += 1
-        else:
-            ## add rounded to list
-            staged_dict[rounded] = 1
-
-    # Language - Bar 
-    lang = tweet_data['lang']
-    if (lang != ''):
-        if (lang in staged_lang):
-            ## increment that lang
-            staged_lang[lang] += 1
-        else:
-            ## add lang to list
-            staged_lang[lang] = 1
-
-    # POS - Bar
-    all_elements = tweet_data['pos']
-    for pos in all_elements:
-        if (pos[0] != ''):
-            if (pos[0] in staged_pos):
-                ## increment that pos
-                staged_pos[pos[0]] += 1
-            else:
-                ## add pos to list
-                staged_pos[pos[0]] = pos[1]
-
-    # # Hyponyms - Scatter 
-    # hyponyms = tweet_data['hyponyms']
-    # if (hyponyms != ''):
-    #     if (hyponyms in staged_hyponyms):
-    #         ## increment that hyponyms
-    #         staged_hyponyms[hyponyms] += 1
-    #     else:
-    #         ## add hyponyms to list
-    #         staged_hyponyms[hyponyms] = 1
 
     # Length - Bar 
     length = tweet_data['tweet length']
@@ -77,16 +36,88 @@ for line in f.readlines():
             ## add length to list
             staged_length[length] = 1
 
+
+    # Language - Bar 
+    lang = tweet_data['lang']
+    if (lang != ''):
+        if (lang in staged_lang):
+            ## increment that lang
+            staged_lang[lang] += 1
+        else:
+            ## add lang to list
+            staged_lang[lang] = 1
+
+
+    # Dictionary - sucess rate bar 
+    try:
+        dict_result = tweet_data['check_dictionary']*100
+        print(dict_result)
+        rounded = round(dict_result)
+        if (rounded in staged_dict):
+            ## increment that rounded
+            staged_dict[rounded] += 1
+        else:
+            ## add rounded to list
+            staged_dict[rounded] = 1
+    except:
+        print("didn't translate this one - dictionary")
+
+
+    # POS - Bar
+    try:
+        all_elements = tweet_data['pos']
+        for pos in all_elements:
+            if (pos[0] != ''):
+                if (pos[0] in staged_pos):
+                    ## increment that pos
+                    staged_pos[pos[0]] += 1
+                else:
+                    ## add pos to list
+                    staged_pos[pos[0]] = pos[1]
+    except:
+        print("didn't translate this one - pos")
+
+
+    # Topic - Bar 
+    try:
+        all_topic = tweet_data['topic']
+        for topic in all_topic:
+            if (topic != ''):
+                if (topic in staged_topic):
+                    ## increment that topic
+                    staged_topic[topic] += 1
+                else:
+                    ## add topic to list
+                    staged_topic[topic] = 1
+    except:
+        print("didn't translate this one - topic")
+
+
     # Named-entity - Scatter
-    all_named = tweet_data['named entity']
-    for named in all_named:
-        if (named[1] != ''):
-            if (named[1] in staged_named):
-                ## increment that named
-                staged_named[named[1]] += 1
-            else:
-                ## add named to list
-                staged_named[named[1]] = 1
+    try:
+        all_named = tweet_data['named entity']
+        for named in all_named:
+            if (named[1] != ''):
+                if (named[1] in staged_named):
+                    ## increment that named
+                    staged_named[named[1]] += 1
+                else:
+                    ## add named to list
+                    staged_named[named[1]] = 1    
+    except:
+        print("didn't translate this one - named entity")
+
+
+    # # Hyponyms - Scatter 
+    # hyponyms = tweet_data['hyponyms']
+    # if (hyponyms != ''):
+    #     if (hyponyms in staged_hyponyms):
+    #         ## increment that hyponyms
+    #         staged_hyponyms[hyponyms] += 1
+    #     else:
+    #         ## add hyponyms to list
+    #         staged_hyponyms[hyponyms] = 1
+
 
     # # Sentiment - Bar 
     # sentiment = tweet_data['sentiment']
@@ -98,16 +129,7 @@ for line in f.readlines():
     #         ## add Sentiment to list
     #         staged_sentiment[sentiment] = 1
 
-    # Topic - Bar 
-    all_topic = tweet_data['topic']
-    for topic in all_topic:
-        if (topic != ''):
-            if (topic in staged_topic):
-                ## increment that topic
-                staged_topic[topic] += 1
-            else:
-                ## add topic to list
-                staged_topic[topic] = 1
+
 
 
 staged_pos = sorted(staged_pos.items(),
@@ -121,15 +143,57 @@ staged_topic = sorted(staged_topic.items(),
 staged_dict = sorted(staged_dict.items(),
                       key=operator.itemgetter(1), reverse=True)
 
-print(staged_pos)
-print(staged_length)
-print(staged_named)
-print(staged_topic)
-print(staged_dict)
+# print(staged_pos)
+# print(staged_length)
+# print(staged_named)
+# print(staged_topic)
+# print(staged_dict)
+
 
 # Visualize Results     
-# data = [go.Bar(
-#     x=pos_x,
-#     y=pos_y
-# )]
-# py.plot(data, filename='basic-bar')
+x_axis=[]
+y_axis=[]
+for pos in staged_pos:
+    x_axis.append(pos[0])
+    y_axis.append(pos[1])
+data = [go.Bar(
+    x=x_axis,
+    y=y_axis
+)]
+py.plot(data, filename='part-of-speech-bar')
+
+# Visualize Results     
+x_axis=[]
+y_axis=[]
+for named in staged_named:
+    x_axis.append(named[0])
+    y_axis.append(named[1])
+data = [go.Bar(
+    x=x_axis,
+    y=y_axis
+)]
+py.plot(data, filename='named-entity-bar')
+
+# Visualize Results     
+x_axis=[]
+y_axis=[]
+for topic in staged_topic:
+    x_axis.append(topic[0])
+    y_axis.append(topic[1])
+data = [go.Bar(
+    x=x_axis,
+    y=y_axis
+)]
+py.plot(data, filename='topics-bar')
+
+# Visualize Results     
+x_axis=[]
+y_axis=[]
+for dict_item in staged_dict:
+    x_axis.append(dict_item[0])
+    y_axis.append(dict_item[1])
+data = [go.Bar(
+    x=x_axis,
+    y=y_axis
+)]
+py.plot(data, filename='dictionary-items-bar')

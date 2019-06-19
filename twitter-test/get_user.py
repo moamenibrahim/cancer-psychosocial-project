9ee0cfcb5736c17dabf8898328d3e59d9d0a8c1f
Moamen Ibrahim <mmibrahem76@gmail.com>
2019-06-19 08:50:28 +0300

    Update get_user.py

--- twitter-test/get_user.py
+++ twitter-test/get_user.py
@@ -6,10 +6,10 @@ from collections import Counter
 import sys
 import json
 
-consumer_key = 'zgwY6GgJ2p6kCX39X17zm4UpK'
-consumer_secret = 'Kv9AazgJmYueIQPmY5kO1MhUsZvDiXaHJZw03fVe9p8H5AipPv'
-access_token = '837798738907312132-p2OZgzDDF7ZeNBMKQ9l5f9XGdMlH1J8'
-access_token_secret = 'wHQFCa7MedvYkF9jtWNtu6rpGMOCXQR7Ptq5jsFKrAbEv'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
 
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)

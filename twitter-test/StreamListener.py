8a9a8d08121fddd536bf6bcbc0c1b30c55a0666c
Moamen Ibrahim <mmibrahem76@gmail.com>
2019-06-19 08:49:59 +0300

    Update StreamListener.py

--- twitter-test/StreamListener.py
+++ twitter-test/StreamListener.py
@@ -6,10 +6,10 @@ from subprocess import call
 from datetime import datetime
 
 
-consumer_key = 'zgwY6GgJ2p6kCX39X17zm4UpK'
-consumer_secret = 'Kv9AazgJmYueIQPmY5kO1MhUsZvDiXaHJZw03fVe9p8H5AipPv'
-access_token = '837798738907312132-p2OZgzDDF7ZeNBMKQ9l5f9XGdMlH1J8'
-access_secret = 'wHQFCa7MedvYkF9jtWNtu6rpGMOCXQR7Ptq5jsFKrAbEv'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_secret = ''
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
